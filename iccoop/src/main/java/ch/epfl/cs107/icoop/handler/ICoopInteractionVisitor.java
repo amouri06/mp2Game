55f453f511c6175bdc576962ec8048d568ee8ae4
Omar Jamoussi <omar.jamoussi@epfl.ch>
2024-11-29 18:31:05 +0100

    yes

--- iccoop/src/main/java/ch/epfl/cs107/icoop/handler/ICoopInteractionVisitor.java
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/handler/ICoopInteractionVisitor.java
@@ -1,7 +1,10 @@
 package ch.epfl.cs107.icoop.handler;
 
 import ch.epfl.cs107.icoop.ICoopBehavior;
+import ch.epfl.cs107.icoop.actor.Explosive;
 import ch.epfl.cs107.icoop.actor.ICoopPlayer;
+import ch.epfl.cs107.icoop.actor.Obstacle;
+import ch.epfl.cs107.icoop.actor.Rock;
 import ch.epfl.cs107.play.areagame.handler.AreaInteractionVisitor;
 
 /**
@@ -13,4 +16,10 @@ public interface ICoopInteractionVisitor extends AreaInteractionVisitor {
     default void interactWith(ICoopBehavior.ICoopCell cell) {}
 
     default void interactWith(ICoopPlayer player) {}
+
+    default void interactWith(Obstacle obstacle) {}
+
+    default void interactWith(Rock rock) {}
+
+    default void interactWith(Explosive explosive) {}
 }
