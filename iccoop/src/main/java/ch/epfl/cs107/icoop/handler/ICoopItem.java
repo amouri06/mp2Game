6051c670c21c63798838c0b5fb2221054e07c3f7
Omar Jamoussi <omar.jamoussi@epfl.ch>
2024-12-06 16:05:05 +0100

    yes

---
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/handler/ICoopItem.java
@@ -0,0 +1,32 @@
+package ch.epfl.cs107.icoop.handler;
+
+import ch.epfl.cs107.icoop.actor.ICoopCellCollectable;
+import ch.epfl.cs107.play.areagame.handler.InventoryItem;
+import ch.epfl.cs107.play.engine.actor.Sprite;
+
+public enum ICoopItem implements InventoryItem {
+    Sword("icoop/sword.icon"),
+    FireKey("icoop/key_red"),
+    WaterKey("icoop/key_blue"),
+    FireStaff( "icoop/staff_fire.icon"),
+    WaterStaff("icoop/staff_water.icon"),
+    Explosive("icoop/explosive"),;
+
+    private String spriteName;
+
+    private ICoopItem(String spriteName) {
+        this.spriteName = spriteName;
+    }
+
+
+    @Override
+    public int getPocketId() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return spriteName;
+    }
+
+}
