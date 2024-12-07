057b69be2ee1f2596dbfa3768c70bb03658ba5d6
Omar Jamoussi <omar.jamoussi@epfl.ch>
2024-12-07 14:51:44 +0100

    yes

--- iccoop/src/main/java/ch/epfl/cs107/icoop/actor/Foe.java
+++ iccoop/src/main/java/ch/epfl/cs107/icoop/actor/Foe.java
@@ -45,7 +45,6 @@ public abstract class Foe extends MovableAreaEntity implements Interactor, Inter
     @Override
     public void update(float deltaTime) {
         if (hp <= 0) {
-            System.out.println("hello");
             deathTimer--;
             deathAnimation.update(deltaTime);
         }
